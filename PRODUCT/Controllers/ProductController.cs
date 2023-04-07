5e9e0e3bfc214f195b9e727daef5c2c3b928853a
EminRzayev <110981070+EminRzayev@users.noreply.github.com>
2023-04-07 22:29:44 +0400

    Add project files.

---
+++ PRODUCT/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRODUCT.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+    }
+}
